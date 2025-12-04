5c9bb44db6a52e07f82185fa258eeeebfbce795b
sara101716 <saravanans@deloitte.com>
2025-12-04 13:32:37 +0530

    sync

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,13 +1,13 @@
 export default function decorate(block){
 const quoteDIV = block.querySelectorAll('scope > div > div');
-const blockquote = document.createElement(tagname,'blockquote');  
+const blockquote = document.createElement(tagname:'blockquote');  
 blockquote.innerhtml = "${quoteDIV.innerhtml}";
 quoteDIV.parentElement.replaceWith(blockquote);
 
 //decorate the author
 const authorDIV = block.querySelectorAll('scope > div > div');
 if(authorDIV){
-  const p = document.createElement(tagname,'p');
+  const p = document.createElement(tagname:'p');
   p.innerhtml = '<em>  - ${authorDIV.innerhtml}</em>';
     authorDIV.parentElement.replaceWith(p);
  }
