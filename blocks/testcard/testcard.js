acb3c744d3bd4a61ffca925949155d5bdac190f2
sara101716 <saravanans@deloitte.com>
2025-12-10 10:55:07 +0530

    done

--- blocks/testcard/testcard.js
+++ blocks/testcard/testcard.js
@@ -4,10 +4,12 @@ export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
+     [...row.children].forEach((col) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
 
     ul.append(li);
+    });
   });
  
   block.replaceChildren(ul);
